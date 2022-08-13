10c8723ab8963a1ff421cee8243934c872e605d9
mhrayhan <mhrayhan430@gmial.com>
2022-08-13 11:32:32 +0600

    cors policy update

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ const { MongoClient, ServerApiVersion, ObjectId, ObjectID } = require('mongodb')
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+const corsOptions = {
+  origin: '*',
+  credentials: true,            //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+}
+app.use(cors(corsOptions));
 app.use(express.json());
 
 
